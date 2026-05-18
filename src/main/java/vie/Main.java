dc288202f781f313fe343c3125f3f4b0ed99bc6a
Yu Siang <alasamakkit@gmail.com>
2026-05-19 04:49:08 +0800

    removed unnecessary files

--- src/main/java/vie/Main.java
+++
@@ -1,7 +0,0 @@
-package vie;
-
-public class Main {
-    public static void main(String[] args) {
-        game.bootstrap.Main.main(args);
-    }
-}
--- src/main/java/vie/engine/rendering/Window.java
+++
@@ -1,5 +0,0 @@
-package vie.engine.rendering;
-
-public class Window {
-    
-}
